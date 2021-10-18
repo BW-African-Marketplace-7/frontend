a15e6bd74cc6f1f8f483a86bc7f0b3028d4839a0
WaylayOvercast <timelyovercast@gmail.com>
2021-10-18 12:35:38 -0500

    added react-router-dom and setup routing for home and about pages

--- React App/src/App.js
+++ React App/src/App.js
@@ -1,12 +1,51 @@
 import './App.css';
+import axios from 'axios';
+import {BrowserRouter as Router}  from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
+import React, {useState, useEffect} from "react";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
 
-    </div>
+  const loginDefault = {
+    username: '',
+    password: ''
+  }
+
+  const [disable, setDisable] = useState(true)
+  const [login,setLogin] = useState(loginDefault)
+
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Link to='/'>Home</Link>
+          <Link to='/about'>about</Link>
+        </header>
+
+        <Route exact path='/'>
+          <section className='upper-content-section'>
+            <div className='top-left'>
+            </div>
+            <div className='top-right'>
+            </div>
+          </section>
+          <section className='content-section'>
+            <div className='mid-left'>
+            </div>
+            <div className='mid-right'>
+            </div>
+          </section>
+          <section className='lower-content-section'>
+            <div className='mid-left'>
+            </div>
+            <div className='mid-right'>
+            </div>
+          </section>
+        </Route>
+      </div>
+    </Router>
   );
 }
 

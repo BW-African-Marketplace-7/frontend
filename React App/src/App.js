aa6210296172e876c6ac5e515b8ced4ee7ccf9a1
WaylayOvercast <timelyovercast@gmail.com>
2021-10-18 14:12:41 -0500

    added button functionality and more logic for login and create pages

--- React App/src/App.js
+++ React App/src/App.js
@@ -12,16 +12,63 @@ function App() {
     username: '',
     password: ''
   }
-
+  const [islogged,setLogged] = useState(false)
   const [disable, setDisable] = useState(true)
   const [login,setLogin] = useState(loginDefault)
+  const [serverRes, setRes] = useState([])
+
+
+  const postValues = (newVal) =>{
+    axios.post('https://reqres.in/api/orders', newVal) /* needs an actual api*/
+    .then(res =>{
+      setRes([res.data,...serverRes])
+      console.log(res)
+    })
+    .catch(valueError => console.error(valueError)
+    )
+    .finally(()=> {
+      setLogin(loginDefault)
+      setLogged(!islogged)
+    })
+  }
+  
+
+  const submit = () => {
+
+    const newForm = {
+      username: login.username.trim(),
+      password: login.password.trim()
+    }
+    postValues(newForm)
+  }
+
+
+
+  const onSub = event =>{
+    event.preventDefault();
+    submit();
+  }
+
+  const update = (name, value) => {
+    //validate(name, value)
+    setLogin({...login, [name]: value});
+  }
+
+  const onUpdate=(evt)=>{
+    const {name, value} = evt.target
+        update(name, value)
+  }
 
   return (
     <Router>
       <div className="App">
         <header className="App-header">
-          <Link to='/'>Home</Link>
-          <Link to='/about'>about</Link>
+          <h1 className='App-h1'>African Marketplace</h1>
+          {islogged ? null: <Link to='/create' className='createbtn btn'>Sign Up</Link>}
+          {islogged ? null:<Link to={islogged ? '/' : '/login'} className='loginbtn btn'>Login</Link>}
+          {islogged ? <button onClick={()=>setLogged(!islogged)}className='logout btn'>Logout</button>: null}
+          <Link to='/' className='homebtn btn'>Home</Link>
+          <Link to='/about' className='aboutbtn btn'>about</Link>
         </header>
 
         <Route exact path='/'>
@@ -43,6 +90,21 @@ function App() {
             <div className='mid-right'>
             </div>
           </section>
+        </Route>
+        <Route path='/login'>
+          <div className='login-form-holder'>
+            <form id='login-form' onSubmit={onSub}>
+              <label>
+                Username:&nbsp;
+                <input 
+                id='username'
+                type='text'
+                onChange={onUpdate}
+                value={login.username}
+                name='username'/>
+              </label>
+            </form>
+          </div>
         </Route>
       </div>
     </Router>

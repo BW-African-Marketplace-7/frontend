020aabff2c2fdbeebb86e75edbd2772e1bb80fee
WaylayOvercast <timelyovercast@gmail.com>
2021-10-20 17:15:53 -0500

    login and signup now communicate over an api and correctly check state arrays for matching values

--- React App/src/Login.js
+++ React App/src/Login.js
@@ -1,5 +1,7 @@
 import { Link, Route } from 'react-router-dom';
-
+import * as yup from 'yup'
+import Schema from './LoginSchema';
+import React, { useEffect } from 'react';
 
 const Create=(props)=>{
 
@@ -7,25 +9,63 @@ const Create=(props)=>{
     login,
     setLogin,
     disable,
-    onSub, } = props
+    setDisable,
+    err,
+    setErr,
+    userList,
+    setLogged } = props
+
+    const checkAcnt =(evt)=>{/* my horrible secondary validation */
+        evt.preventDefault()
+        const user = document.getElementById('username')
+        const email = document.getElementById('email')
+        const pass = document.getElementById('password')
+        userList.map(act =>{
+           if(act.username === user.value && act.email === email.value && act.password === pass.value){
+                setLogged(true);
+                return window.location.pathname = '/home'
+            }else{ 
+                console.log('user does not exist')
+            }   
+        })
+    }
+    const success=(evt)=>{
+        evt.preventDefault() && setLogged(true)
+    }
     
     const onUpdate=(evt)=>{
         const {name, value} = evt.target
             update(name, value)
     }
+
+    const validate = (name,value) => {
+        yup.reach(Schema, name)
+        .validate(value)
+        .then(() => setErr({...err, [name]: ''}))
+        .catch(err => setErr({...err, [name]: err.errors[0] }))
+    }
     
     const update = (name, value) => {
-    /*validate(name, value) needs to be setup through schema */
-    setLogin({...login, [name]: value});
+
+        validate(name, value)
+        setLogin({...login, [name]: value});
+
     }
+
+    useEffect(()=>{
+
+        Schema.isValid(login).then(valid =>
+        setDisable(!valid))
+
+    },[login])
 
 
 return(
 <Route path='/login'>
     <div className='login-form-holder'>
-        <form id='login-form' onSubmit={onSub}>
+        <form id='login-form' onSubmit={checkAcnt}>
         <label>
-            Username:&nbsp;
+            Username: &nbsp;
             <input 
             id='username'
             type='text'
@@ -34,7 +74,16 @@ return(
             name='username'/>
         </label>
         <label>
-        Password:&nbsp;
+            Enter a valid email:  &nbsp;
+            <input 
+            id='email'
+            type='email'
+            onChange={onUpdate}  
+            value={login.email}
+            name='email'/>
+        </label>
+        <label>  
+        Password: &nbsp;
             <input 
             id='password'
             type='password'
@@ -55,4 +104,6 @@ return(
 )}
 
 
+
 export default Create
+     
